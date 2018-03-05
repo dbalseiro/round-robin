d485cac9515cdcdaa05be439c649a196bb17919b
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2018-03-05 01:51:41 -0500

    It doesn't make sense

--- src/RoundRobin.hs
+++ src/RoundRobin.hs
@@ -14,14 +14,47 @@ data Survey = Survey
   } deriving (Show, Eq, Ord)
 
 roundRobin :: [Survey] -> Survey
-roundRobin l = snd $ maximum $ map (\s -> (getStaticWeight (length l + 1) s * getDynamicWeight s, s)) l
+roundRobin l = snd . maximum $ map foo l
+  where
+    foo s = (staticWeight * dynamicWeight, s)
+      where
+        staticWeight = getStaticWeight (length l + 1) s
+        dynamicWeight = getDynamicWeight s
 
+-- l = 5
+-- r = 2, 1, 3, 0
+--   = 3, 4, 2, 1
 getStaticWeight :: Int -> Survey -> Double
-getStaticWeight l Survey{..} = fromIntegral $ l - (if surveyRank == 0 then l else surveyRank)
+getStaticWeight n Survey{..} = fromIntegral $ n - rank
+  where
+    rank = if surveyRank /= 0 then surveyRank else n - 1
 
+-- q = 100, 100,  80, 150
+-- s =   0,   0,   0,   0
+--   =   1,   1,   1,   1
 getDynamicWeight :: Survey -> Double
-getDynamicWeight Survey{..} = fromIntegral surveyRespondentStarted / fromIntegral surveyOverallQuota
+getDynamicWeight Survey{..} =
+  let n = fromIntegral surveyRespondentStarted / fromIntegral surveyOverallQuota
+  in if n == 0 then 0.001 else
+     if n == 1 then 0 else n
 
+updateSurvey :: Survey -> Survey
+updateSurvey Survey{..} = Survey
+  surveyId
+  surveyScore
+  surveyOverallQuota
+  (surveyRespondentStarted + 1)
+  surveyRank
+
+testRR :: [Int]
+testRR = rrSurveys 300 generateSurveys
+
+rrSurveys :: Int -> [Survey] -> [Int]
+rrSurveys 0 _ = []
+rrSurveys n surveys =
+  let rrSurvey = roundRobin surveys
+      updatedSurveys = updateSurvey rrSurvey : filter (rrSurvey /=) surveys
+  in surveyId rrSurvey : rrSurveys (n - 1) updatedSurveys
 
 generateSurveys :: [Survey]
 generateSurveys =
@@ -48,7 +81,7 @@ generateSurveys =
     }
   , Survey
     { surveyId                = 4
-    , surveyScore             = 1.1
+    , surveyScore             = 0.7
     , surveyOverallQuota      = 150
     , surveyRespondentStarted = 0
     , surveyRank              = 0
