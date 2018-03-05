ce56bd6e17901f8f939a7c6d71cca0680540e6ee
Diego Balseiro <dbalseiro@stackbuilders.com>
2018-03-05 15:38:47 -0500

    second version of RR

--- src/RoundRobin.hs
+++ src/RoundRobin.hs
@@ -11,6 +11,7 @@ data Survey = Survey
   , surveyOverallQuota      :: Int
   , surveyRespondentStarted :: Int
   , surveyRank              :: Int
+  , surveyWeight            :: Maybe Double
   } deriving (Show, Eq, Ord)
 
 roundRobin :: [Survey] -> Survey
@@ -20,7 +21,53 @@ getStaticWeight :: Int -> Survey -> Double
 getStaticWeight l Survey{..} = fromIntegral $ l - (if surveyRank == 0 then l else surveyRank)
 
 getDynamicWeight :: Survey -> Double
-getDynamicWeight Survey{..} = fromIntegral surveyRespondentStarted / fromIntegral surveyOverallQuota
+getDynamicWeight Survey{..} = 1 - fromIntegral surveyRespondentStarted / fromIntegral surveyOverallQuota
+
+roundRobin2 :: [Survey] -> Survey
+roundRobin2 =
+  let totalRank = sum (map surveyRank generateSurveys)
+      totalQuotaPercentage s = (fromIntegral $ surveyOverallQuota s) / fromIntegral (sum (map surveyOverallQuota generateSurveys))
+      weight = getStaticWeight (length generateSurveys + 1) / fromIntegral totalRank
+      load s = (getDynamicWeight s * totalQuotaPercentage)
+   in snd . maximum . map (\s ->
+                              let w = load s * weight s
+                               in (w, s { surveyWeight = Just w
+                                        , surveyRespondentStarted = surveyRespondentStarted s + 1
+                                        }
+                              ))
+
+
+prettyPrintSurvey :: Survey -> IO ()
+prettyPrintSurvey Survey{..} = do
+  putStrLn
+    $  "ID="
+    ++ show surveyId
+    ++ " Corrected="
+    ++ show surveyWeight
+
+rrSurveys :: Int -> [Survey] -> IO [Survey]
+rrSurveys 0 _ = return []
+rrSurveys i ss = do
+  let rrs = roundRobin2 ss
+      rest = filter ((/= surveyId rrs) . surveyId) ss
+
+  mapM_ prettyPrintSurvey ss
+  putStrLn "-------------------------------------------------"
+  rrSurveys (i - 1) (rrs:rest)
+
+updateSurvey :: Survey -> Survey
+updateSurvey s = s
+  { surveyRespondentStarted = surveyRespondentStarted s + 1 }
+
+testRR :: Int -> IO ()
+testRR n = void $ rrSurveys n generateSurveys
+
+prettyPrint :: Survey -> IO ()
+prettyPrint survey = return ()
+    {-putStrLn-}
+      {-$  "ID="-}
+      {-++ show (surveyId survey)-}
+
 
 
 generateSurveys :: [Survey]
@@ -31,6 +78,7 @@ generateSurveys =
     , surveyOverallQuota      = 100
     , surveyRespondentStarted = 0
     , surveyRank              = 2
+    , surveyWeight            = Nothing
     }
   , Survey
     { surveyId                = 2
@@ -38,22 +86,33 @@ generateSurveys =
     , surveyOverallQuota      = 100
     , surveyRespondentStarted = 0
     , surveyRank              = 1
+    , surveyWeight            = Nothing
     }
   , Survey
     { surveyId                = 3
     , surveyScore             = 1.1
-    , surveyOverallQuota      = 80
+    , surveyOverallQuota      = 100
     , surveyRespondentStarted = 0
     , surveyRank              = 3
+    , surveyWeight            = Nothing
     }
+  ]
+  {-
   , Survey
     { surveyId                = 4
     , surveyScore             = 1.1
     , surveyOverallQuota      = 150
     , surveyRespondentStarted = 0
-    , surveyRank              = 0
+    , surveyRank              = 4
+    }
+  , Survey
+    { surveyId                = 5
+    , surveyScore             = 1.1
+    , surveyOverallQuota      = 150
+    , surveyRespondentStarted = 0
+    , surveyRank              = 5
     }
   ]
-
+  -}
 randomSurvey :: IO Survey
 randomSurvey = undefined
