0d19093109743300e18a1fa1b7d8a1da9b9dae7b
Diego Balseiro <dbalseiro@stackbuilders.com>
2018-03-06 16:30:00 -0500

    full version of round robin

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,34 @@
+{-# LANGUAGE ImplicitParams #-}
+
 module Main where
+
+import           System.Environment
+import           Text.Read
 
 import           RoundRobin
 
 main :: IO ()
-main = testRR 60
+main = do
+  args <- getArgs
+  checkArgs args
+
+  let ?select = selectMethod (args !! 1)
+  let respondents = getRespondents (args !! 0)
+
+  testRR respondents
+
+checkArgs :: [String] -> IO ()
+checkArgs args
+  | length args /= 2 = error "Incorrect args"
+  | otherwise = return ()
+
+selectMethod :: String -> ([(Double, Survey)] -> IO Survey)
+selectMethod "random" = selectSurvey
+selectMethod "fixed"  = selectSurveyMax
+selectMethod _        = error "Incorrect method"
+
+getRespondents :: String -> Int
+getRespondents r =
+  case readMaybe r of
+    Nothing -> error "Incorrect respondents"
+    Just x  -> x
--- src/RoundRobin.hs
+++ src/RoundRobin.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ImplicitParams  #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module RoundRobin where
@@ -8,7 +9,6 @@ import           System.Random
 
 data Survey = Survey
   { surveyId                :: Int
-  , surveyScore             :: Double
   , surveyOverallQuota      :: Int
   , surveyRespondentStarted :: Int
   , surveyRank              :: Int
@@ -24,14 +24,13 @@ getStaticWeight l Survey{..} = fromIntegral $ l - (if surveyRank == 0 then l els
 getDynamicWeight :: Survey -> Double
 getDynamicWeight Survey{..} = 1 - fromIntegral surveyRespondentStarted / fromIntegral surveyOverallQuota
 
-roundRobin2 :: [Survey] -> IO Survey
+roundRobin2 :: (?select :: [(Double, Survey)] -> IO Survey) => [Survey] -> IO Survey
 roundRobin2 = do
   let totalRank = sum (map surveyRank generateSurveys)
-      totalQuotaPercentage s = (fromIntegral $ surveyOverallQuota s) / fromIntegral (sum (map surveyOverallQuota generateSurveys))
       weight s = (getStaticWeight (length generateSurveys + 1) s) / fromIntegral totalRank
       load s = getDynamicWeight s
 
-  selectSurveyMax . map (\s ->
+  ?select . map (\s ->
                               let w = weight s * load s
                                in (w, s { surveyWeight = Just w
                                       , surveyRespondentStarted = surveyRespondentStarted s + 1
@@ -62,68 +61,30 @@ prettyPrintSurvey Survey{..} = do
     ++ "\tRespondents="
     ++ show surveyRespondentStarted
 
-rrSurveys :: Int -> [Survey] -> IO [Survey]
-rrSurveys 0 _ = return []
+rrSurveys :: (?select :: [(Double, Survey)] -> IO Survey) => Int -> [Survey] -> IO [Survey]
+rrSurveys 0 x = return x
 rrSurveys i ss = do
   rrs <- roundRobin2 ss
   let rest = filter ((/= surveyId rrs) . surveyId) ss
 
-  {-putStrLn $ "Run " ++ show i-}
-  {-mapM_ prettyPrintSurvey ss-}
-  {-putStrLn "-------------------------------------------------"-}
-  prettyPrintSurvey rrs
+  putStrLn $ (show $ surveyId rrs) ++ "\t"
   rrSurveys (i - 1) (rrs:rest)
 
 updateSurvey :: Survey -> Survey
 updateSurvey s = s
   { surveyRespondentStarted = surveyRespondentStarted s + 1 }
 
-testRR :: Int -> IO ()
+testRR :: (?select :: [(Double, Survey)] -> IO Survey) => Int -> IO ()
 testRR n = void $ rrSurveys n generateSurveys
 
 generateSurveys :: [Survey]
-generateSurveys =
-  [ Survey
-    { surveyId                = 1
-    , surveyScore             = 1.1
-    , surveyOverallQuota      = 10
-    , surveyRespondentStarted = 0
-    , surveyRank              = 2
-    , surveyWeight            = Nothing
-    }
-  , Survey
-    { surveyId                = 2
-    , surveyScore             = 1.2
-    , surveyOverallQuota      = 20
-    , surveyRespondentStarted = 0
-    , surveyRank              = 1
-    , surveyWeight            = Nothing
-    }
-  , Survey
-    { surveyId                = 3
-    , surveyScore             = 1.1
-    , surveyOverallQuota      = 30
-    , surveyRespondentStarted = 0
-    , surveyRank              = 3
-    , surveyWeight            = Nothing
-    }
-  ]
-  {-
-  , Survey
-    { surveyId                = 4
-    , surveyScore             = 1.1
-    , surveyOverallQuota      = 150
-    , surveyRespondentStarted = 0
-    , surveyRank              = 4
-    }
-  , Survey
-    { surveyId                = 5
-    , surveyScore             = 1.1
-    , surveyOverallQuota      = 150
-    , surveyRespondentStarted = 0
-    , surveyRank              = 5
-    }
-  ]
-  -}
-randomSurvey :: IO Survey
-randomSurvey = undefined
+generateSurveys = map mkSurvey [1..20]
+
+mkSurvey :: Int -> Survey
+mkSurvey i = Survey
+  { surveyId                = i
+  , surveyOverallQuota      = 100
+  , surveyRespondentStarted = 0
+  , surveyRank              = i
+  , surveyWeight            = Nothing
+  }
